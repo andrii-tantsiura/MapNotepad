d2701dad9483f23262e5c9d1dccb24824a5fab23
andrii.tantsiura <andrii.tantsiura@headworks.com.ua>
2023-05-23 19:48:24 +0300

    task-4. added clustering

--- screens/Home/MapScreen/index.tsx
+++ screens/Home/MapScreen/index.tsx
@@ -1,15 +1,25 @@
 import { FC, useEffect, useRef } from "react";
 import { View } from "react-native";
-import MapView, { Marker, Region } from "react-native-maps";
+import MapView from "react-native-map-clustering";
+import { Marker, Region } from "react-native-maps";
 import { useSelector } from "react-redux";
 
 import { LOCATION_ICON, MARKER_ICON } from "../../../assets/icons";
 import { IconButton } from "../../../components/common";
+import COLORS from "../../../constants/colors";
 import { IconButtonStyles } from "../../../constants/globalStyles";
+import { FontWeights } from "../../../constants/typography";
 import { useCurrentLocation } from "../../../hooks/useCurrentLocation";
 import { TabProps } from "../../../navigation/TabStack/types";
 import { selectPins } from "../../../store/redux/slices/pinsSlice";
 import styles from "./styles";
+
+const INITIAL_REGION = {
+  latitude: 6.394999333003437,
+  latitudeDelta: 136.53565015480194,
+  longitude: -4.873477723449469,
+  longitudeDelta: 144.64280527085066,
+};
 
 export const MapScreen: FC<TabProps> = () => {
   const mapViewRef = useRef<MapView>(null);
@@ -26,13 +36,21 @@ export const MapScreen: FC<TabProps> = () => {
         ...currentLocation,
       };
 
+      // NOTE: perhaps this warning is a bug
       mapViewRef.current?.animateToRegion(region);
     }
   }, [currentLocation]);
 
   return (
     <View style={styles.container}>
-      <MapView ref={mapViewRef} style={styles.map}>
+      <MapView
+        clusterColor={COLORS.lightPrimary}
+        clusterFontFamily={FontWeights.regular.fontFamily}
+        clusterTextColor={COLORS.systemWhite}
+        style={styles.map}
+        ref={mapViewRef}
+        initialRegion={INITIAL_REGION}
+      >
         {favoritePins.map((pin, index) => (
           <Marker
             image={MARKER_ICON}
