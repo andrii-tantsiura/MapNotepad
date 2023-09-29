ee6dd81e76c726ff47cc1a9b417bcd8a756c664a
DESKTOP-R152FD0\andrew <andreytancyura@gmail.com>
2023-09-29 12:04:38 +0300

    task-15. irrelevant code in App.Routes has been removed

--- navigation/App.routes.tsx
+++ navigation/App.routes.tsx
@@ -4,15 +4,11 @@ import {
   Theme,
 } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
 import FlashMessage from "react-native-flash-message";
-import { useSelector } from "react-redux";
 
 import { LoaderView } from "../components/sections";
-import { AppColors, DarkAppColors, LightAppColors } from "../constants";
-import { AppThemes } from "../enums";
+import { AppColors } from "../constants";
 import { useFirebaseLogin } from "../hooks";
-import { selectSettings } from "../store/redux/slices/settingsSlice";
 import AuthStack from "./AuthStack";
 import HomeStack from "./HomeStack";
 
@@ -20,23 +16,18 @@ type AppRoutesProps = {
   onReady: () => void;
 };
 
+const theme: Theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    card: AppColors.background,
+    text: AppColors.systemGray,
+    background: AppColors.background,
+  },
+};
+
 const AppRoutes: React.FC<AppRoutesProps> = ({ onReady }) => {
   const { isLoginInProcess, isAuthenticated } = useFirebaseLogin();
-
-  const { appTheme } = useSelector(selectSettings);
-  const [theme, setTheme] = useState<Theme>();
-
-  useEffect(() => {
-    setTheme({
-      ...DefaultTheme,
-      colors: {
-        ...DefaultTheme.colors,
-        card: AppColors.background,
-        text: AppColors.systemGray,
-        background: AppColors.background,
-      },
-    });
-  }, [appTheme]);
 
   return isLoginInProcess ? (
     <LoaderView />
